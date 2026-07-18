6dbf66aa9da30e6cfc13ab3f0865e014c177492b
Hamza Ikram <hamzaikram2026@gmail.com>
2026-07-18 20:48:33 +0530

    Update Streamlit app and deployment files

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ col1, col2, col3 = st.columns([1, 1, 1])
 with col1:
     st.link_button(
         "Dataset",
-        "https://github.com/hamzaikram2026/Pharmaceutical-Inventory-Demand-Forecasting-using-Machine-Learning/data", use_container_width=True
+        "https://github.com/hamzaikram2026/Pharmaceutical-Inventory-Demand-Forecasting-using-Machine-Learning/tree/main/data", use_container_width=True
     )
 
 with col2:
